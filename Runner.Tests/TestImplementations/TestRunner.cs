0714314fd691f69f16d75da0dd96e9b98ee8a843
Benjamin Pannell <admin@sierrasoftworks.com>
2015-03-11 08:59:27 +0200

    Added a test runner for the runner

---
+++ Runner.Tests/TestImplementations/TestRunner.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Marathon.Tests.TestImplementations
+{
+    class TestRunner : Marathon.Runner
+    {
+        public TestRunner(string[] args)
+            : base(args)
+        {
+
+        }
+
+        protected override void Initialize()
+        {
+            base.Initialize();
+            Network = new TestNetwork(this);
+        }
+    }
+}
